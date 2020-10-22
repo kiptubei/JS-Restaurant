26c8b9cbfd023acc5dfd5714751e46bb6610ee61
kiptubei <kiptubei@gmail.com>
2020-10-22 10:45:28 +0300

    Added Readme file and screenshots

---
+++ README.md
@@ -0,0 +1,77 @@
+# JS_Restaurant
+
+Simple restaurant homepage!
+
+# Project Specification
+
+1. Use of webpack
+2. Use of ES6 methods
+
+## Built With
+
+- HTML5
+- CSS
+- Javascript ES6
+
+## screenshots
+
+### Index Page
+
+![screenshot](assets/images/Screenshot.png)
+
+## Getting Started
+
+To get a local copy up and running follow these steps:
+
+1. clone the project
+
+```bash
+# Clone this repository
+$ git clone https://github.com/kiptubei/JS-Restaurant.git
+# Go into the repository
+$ cd RestaurantJS
+# start the app
+Copy the absolute path of the index file, paste on your browser and click on enter
+```
+
+### Prerequisites
+
+Mozilla Firefox
+
+- Javascript enabled
+
+### Usage
+
+- Fork/Clone this project to your local machine
+- Open folder in your local enviroment and run these lines of code to get started:
+- Double click on index.html
+- To view in IDE just open project folder in your prefered IDE
+
+## Authors
+
+👤 **Mark James Kiptubei**
+
+- Github: [@kiptubei](https://github.com/kiptubei)
+- Twitter: [@mjabei](https://twitter.com/mjabei)
+- Linkedin: [Mark James Kiptubei](https://www.linkedin.com/in/mark-james-k-aa875829/)
+
+
+## 🤝 Contributing
+
+Contributions and feature requests are welcome!
+
+Start by:
+
+- Forking the project
+- Cloning the project to your local machine
+- `cd` into the project directory
+- Run `git checkout -b your-branch-name`
+- Make your contributions
+- Push your branch up to your forked repository
+- Open a Pull Request with a detailed description to the development(or master if not available) branch of the original project for a review
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,173 @@
-console.log("hello web");
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+// CONCATENATED MODULE: ./src/navbar.js
+const Navbar = () => {
+  const navbar = document.createElement("nav");
+  navbar.className = "navbar";
+  navbar.id = "navbar";
+
+  const logo = document.createElement("img");
+  logo.className = "logo";
+  logo.src = "assets/images/logo.png";
+  logo.id = "logo";
+  navbar.appendChild(logo);
+
+  const links = document.createElement("ul");
+  links.className = "nav_links";
+  navbar.appendChild(links);
+
+  const home = document.createElement("li");
+  home.className = "link";
+  home.innerHTML = "Home";
+  home.id = "home";
+  links.appendChild(home);
+
+  const menu = document.createElement("li");
+  menu.className = "link";
+  menu.innerHTML = "Menu";
+  menu.id = "menu";
+  links.appendChild(menu);
+
+  const contact = document.createElement("li");
+  contact.className = "link";
+  contact.innerHTML = "Contact";
+  contact.id = "contact";
+  links.appendChild(contact);
+
+  return navbar;
+};
+
+// CONCATENATED MODULE: ./src/contact.js
+const contact = () => {
+  const conatctinfo = document.createElement("div");
+  conatctinfo.className = "conatctinfo";
+  conatctinfo.id = "conatctinfo";
+
+  const email = document.createElement("h2");
+  email.className = "email";
+  email.innerHTML = "email : foodbazaar@gmail.com";
+  conatctinfo.appendChild(email);
+
+  const phone_number = document.createElement("h2");
+  phone_number.className = "phonenumber";
+  phone_number.innerHTML = "Phone Number : +254720111111";
+  conatctinfo.appendChild(phone_number);
+
+  const location = document.createElement("h2");
+  location.className = "location";
+  location.innerHTML = "Location : Eldoret,Kenya";
+  conatctinfo.appendChild(location);
+
+  return conatctinfo;
+};
+
+// CONCATENATED MODULE: ./src/menu.js
+const list = [
+  {
+    name: "Pizza",
+    img: "assets/images/pizza.jpg",
+    desc: "Best Pizza in Town",
+  },
+  {
+    name: "Soup",
+    img: "assets/images/soup.jpg",
+    desc: "Spicy or Frothy",
+  },
+  {
+    name: "Salad",
+    img: "assets/images/salad.jpg",
+    desc: "Farm fresh salads",
+  },
+  {
+    name: "Chicken",
+    img: "assets/images/chicken.jpg",
+    desc: "Authentic kienyeji chicken available",
+  },
+];
+const createcard = (item) => {
+  const container = document.createElement("div");
+  container.className = "card";
+  container.id = "menucards";
+
+  const img = document.createElement("img");
+  img.src = item.img;
+  img.className = "foodimg";
+  container.appendChild(img);
+
+  const name = document.createElement("h3");
+  name.innerHTML = item.name;
+  name.className = "foodname";
+  container.appendChild(name);
+
+  const desc = document.createElement("p");
+  desc.innerHTML = item.desc;
+  desc.className = "fooddesc";
+  container.appendChild(desc);
+
+  return container;
+};
+const menu = () => {
+  const foodlist = document.createElement("div");
+  foodlist.className = "foodlist";
+  foodlist.id = "foodlist";
+
+  const foodlist_title = document.createElement("h1");
+  foodlist_title.className = "foodlist_title";
+  foodlist_title.innerHTML = "our list of today :";
+  foodlist_title.id = "foodlist_title";
+  foodlist.appendChild(foodlist_title);
+  list.forEach((item) => {
+    foodlist.appendChild(createcard(item));
+  });
+
+  return foodlist;
+};
+
+// CONCATENATED MODULE: ./src/home.js
+
+const home = () => {
+  const title = document.createElement("h1");
+  title.className = "title";
+  title.innerHTML = "The Food Bazaar";
+  title.id = "title";
+  return title;
+};
+
+// CONCATENATED MODULE: ./src/index.js
+
+const root = document.getElementById("content");
+const navbar = Navbar();
+root.appendChild(navbar);
+const links = document.querySelectorAll(".link");
+
+const page = document.createElement("div");
+page.className = "page";
+page.id = "page";
+
+page.appendChild(home());
+root.appendChild(page);
+
+const roting = (link) => {
+  if (link === "contact") {
+    page.className = "page";
+    return contact();
+  } else if (link === "home") {
+    page.className = "page";
+    return home();
+  } else if (link === "menu") {
+    page.className = "menupage";
+
+    return menu();
+  }
+};
+links.forEach((li) => {
+  li.addEventListener("click", (e, id = li.id) => {
+    let element = roting(id);
+    page.innerHTML = "";
+    page.appendChild(element);
+  });
+});
+
+/******/ })()
+;
